b4c5096594bc5f322241639b939748376d439cc7
Ankush <ankushgoyal11@gmail.com>
2019-05-29 09:58:57 -0400

    Initial commit

---
+++ src/main/java/com/springpractice/dipractice/DiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springpractice.dipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiPracticeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/springpractice/dipractice/controller/MyController.java
@@ -0,0 +1,4 @@
+package com.springpractice.dipractice.controller;
+
+public class MyController {
+}
